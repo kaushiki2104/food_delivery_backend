ed8dc3f5e5a8452650ecad7b7cec6ef545a6f5d8
Kaushiki Singh <kaushikisingh2104@gmail.com>
2026-05-17 20:40:44 +0530

    Index file change in feature

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
   cors:{
-  origin:"http://localhost:5173",
+  origin:"http://localhost:5173" || "https://delivery-webs.netlify.app",
   credentials:true,
   methods:['POST','GET']
 }
